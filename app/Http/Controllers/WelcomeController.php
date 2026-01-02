b2dbef9aaa1cce5e1c7ef93ab7db3b2114268ac4
sosuke-iwabuchi-Satellite <sosuke.iwabuchi@satellite-tech.co.jp>
2026-01-02 09:27:04 +0900

    feat: replace anonymous route closures with dedicated invokable controllers for welcome and dashboard routes.

---
+++ app/Http/Controllers/WelcomeController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use Illuminate\Foundation\Application;
+use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
+use Inertia\Response;
+
+class WelcomeController extends Controller
+{
+    /**
+     * Handle the incoming request.
+     */
+    public function __invoke(Request $request): Response
+    {
+        return Inertia::render('Welcome', [
+            'canLogin' => Route::has('login'),
+            'canRegister' => Route::has('register'),
+            'laravelVersion' => Application::VERSION,
+            'phpVersion' => PHP_VERSION,
+        ]);
+    }
+}
