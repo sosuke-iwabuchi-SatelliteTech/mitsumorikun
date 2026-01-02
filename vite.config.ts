53c90bd395c662cf68c1967d13740aadda3cd304
sosuke-iwabuchi-Satellite <sosuke.iwabuchi@satellite-tech.co.jp>
2026-01-02 12:27:38 +0900

    config: Add Vite server host and HMR settings and specify `php artisan serve` port.

--- vite.config.ts
+++ vite.config.ts
@@ -10,4 +10,12 @@ export default defineConfig({
         }),
         react(),
     ],
+    server: {
+        host: '0.0.0.0',
+        port: 5173,
+        strictPort: true,
+        hmr: {
+            host: 'localhost',
+        },
+    },
 });
