9cc5328b02bd4ccaaee6e2a8940eed9781f7b7b4
sosuke-iwabuchi-Satellite <sosuke.iwabuchi@satellite-tech.co.jp>
2026-01-18 23:25:45 +0900

    migrationミス補正

--- database/migrations/2026_01_01_144820_create_customers_table.php
+++ database/migrations/2026_01_01_144820_create_customers_table.php
@@ -15,11 +15,15 @@ return new class extends Migration
             $table->uuid('id')->primary();
             $table->foreignUuid('user_group_id')->constrained()->cascadeOnDelete();
             $table->string('name');
+            $table->string('contact_person_name')->nullable();
             $table->string('zip_code')->nullable();
+            $table->string('address')->nullable();
             $table->string('address1')->nullable();
             $table->string('address2')->nullable();
             $table->string('phone_number')->nullable();
+            $table->string('fax_number')->nullable();
             $table->string('email')->nullable();
+            $table->text('remarks')->nullable();
             $table->timestamps();
             $table->softDeletes();
         });
